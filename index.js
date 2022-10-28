8819e954ef4d4c4e3e13b0f8eaa9cf70d198d577
LorenaLampis <lampisml@gmail.com>
2022-10-27 23:16:52 -0300

    fixed names

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const btncomprar = document.getElementById("comprartickets")
 
 function gohome(){
-    window.location.href = './pages/Comprar tickets.html'
+    window.location.href = './ComprarTickets/Comprar tickets.html'
 }
 
 btncomprar.onclick = (e) => {gohome(e)}
